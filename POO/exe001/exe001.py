dcea713293401354e6d7605a7923a64ad75c4069
paulovictor1997 <paulovictornunes97@gmail.com>
2026-01-23 11:24:47 -0300

    Create exe001

---
+++ POO/exe001/exe001.py
@@ -0,0 +1,20 @@
+#DECLARAÇÃO DE CLASSE
+class Pessoa :
+    def __init__(self) : #Método construtor
+        #Atributos de instância
+        self.nome = ""
+        self.idade = 0
+
+    #Métodos de instância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"Me chamo {self.nome} e tenho {self.idade} anos !"
+
+#DECLARAÇÃO DE OBJETOS - Chamando o método construtor
+pessoa1 = Pessoa()
+pessoa1.nome = "Paulo Victor"
+pessoa1.idade = 28
+pessoa1.aniversario()
+print(pessoa1.mensagem())
