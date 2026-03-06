2b923cac138245973b61771ef041c06e3c8c6fa3
paulovictor1997 <paulovictornunes97@gmail.com>
2026-03-06 17:16:21 -0300

    Update
    Atualizando código do desafio 03

--- POO/Desafios/desafio03.py
+++ POO/Desafios/desafio03.py
@@ -4,40 +4,42 @@ quanto de carne deve ser comprado, o custo total do churrasco e no final o preç
 
 """
 # Criando valores fixos para facilitar o exercicio
-# 400g de carne por pessoa (0,4 kg) / Preço do kg da carne: R$ 50,00
+# 400g de carne por pessoa (0,4 kg) / Preço do kg da carne: R$ 82,40
 from rich import print
+from rich.panel import Panel
 
 class Churrasco : 
-    def __init__(self, qtd_pessoas):
-        if qtd_pessoas <=0: 
-            raise ValueError ("Adicione ao menos uma pessoa") #Caso passe o número zerado de pessoas, o código irá mostrar esse erro
-        self.qtd_pessoas = qtd_pessoas
-        self.consumo_por_pessoa = 0.4 
-        self.preco_kg = 50.0
-    
-    def calcular_carne(self):
-        return self.qtd_pessoas * self.consumo_por_pessoa
-    
-    def custo_total(self):
-        return self.calcular_carne() * self.preco_kg
-    
-    def calculo_por_pessoa(self):
-        return self.custo_total() / self.qtd_pessoas
-    
-    def resumo(self):
-        print("=== Churrasco ===")
-        print(f"Quantidade de pessoas :[blue]{self.qtd_pessoas}[/blue]")
-        print(f"Carne necessária : [blue] {self.calcular_carne():.2f} KG[/blue]")
-        print(f"Custo total : [blue] R$ {self.custo_total():.2f}[/blue]")
-        print(f"Valor por pessoa : [blue] R$ {self.calculo_por_pessoa():.2f}[/blue]")
-try:
-    churrasco = Churrasco(10)  
-    churrasco.resumo()
-except ValueError as erro:
-    print("=" * 40)
-    print("[bold red]ERRO NO CHURRASCO[/bold red]")
-    print(f"Motivo: {erro}")
-    print("=" * 40)
+        #Atributo de classe 
+        consumo:float = 0.4
+        preco_kg = 82.40
 
-#churrasco = Churrasco(10) 
-#churrasco.resumo()
+        def __init__(self, titulo, quant):
+        #Atributo de instância
+            self.titulo = titulo
+            self.praticipantes = quant
+
+        def __str__(self):
+            return  f"Esse é o {self.titulo} com {self.praticipantes} pessoas participando !"
+        
+        #Métodos de calculo
+        def calcular_quant_carne(self) -> float:
+            return self.praticipantes * Churrasco.consumo
+
+        def calcular_custo_total(self) -> float:
+            return self.calcular_quant_carne() * self.preco_kg
+        
+        def custoIdividual(self) -> float:
+            return self.calcular_custo_total() / self.praticipantes
+        
+        def analisar (self):
+            conteudo = f"Analisando [green]{self.titulo}[/] com [blue]{self.praticipantes} convidados[/]."
+            conteudo += f"\nCada participante comerá {Churrasco.consumo}KG e cada KG custa R${Churrasco.preco_kg:.2f}"
+            conteudo += f"Recomendo comprar [blue]{self.calcular_quant_carne():.3f}[/] KG de carne."
+            conteudo += f"\nO custo total é de : [yellow] R$ {self.calcular_custo_total():.2f} [/]"
+            conteudo += f"\nCada pessoa pagará  :[yellow] R${self.custoIdividual():.2f} [/]"
+            painel = Panel(conteudo, title=self.titulo)
+            print(painel)
+    
+
+c = Churrasco(titulo="Churrasco dos amigos", quant=15)
+c.analisar()
